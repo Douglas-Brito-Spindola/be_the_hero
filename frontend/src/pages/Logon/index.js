a100e8757500cd4a364dc687f5514b3058a48159
Douglas Spíndola <douglas.spindola15@gmail.com>
2020-05-23 23:57:50 -0300

    { Ajuste } Aplicado class de css nos elmentos html

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-
+import { FiLogIn} from 'react-icons/fi';
 import './styles.css';
+import '../../global.css';
 
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
@@ -15,8 +16,9 @@ export default function Logon(){
                     <h1> Faça seu logon </h1>
 
                     <input placeholder="Sua ID" />
-                    <button type="submit">Entrar</button>
+                    <button className="button" type="submit">Entrar</button>
                     <a href="/register">
+                        <FiLogIn size={16} color="#E02041" />
                         Não tenho cadastro
                     </a>
                 </form>
