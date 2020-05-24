e5feca5f4a519611d9630bea0d7b35865395a014
Douglas Spíndola <douglas.spindola15@gmail.com>
2020-05-24 13:21:16 -0300

    { Criação } Criado forms register

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -16,13 +16,22 @@ export default function Register() {
 
            <h1>Cadastro</h1>
             <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos de sua ONG.</p>
-            <Link className="back-link" href="/register">
+            <Link className="back-link" to="/">
                 <FiArrowLeft size={16} color="#E02041" />
                 Não tenho cadastro
             </Link>
            </section>
 
            <form>
+               <input placeholder="Nome da ONG" />
+               <input type="email" placeholder="E-mail" />
+               <input placeholder="WhatsApp" />
+
+               <div className="input-group">
+                   <input placeholder="Cidade"></input>
+                   <input placeholder="UF" style={{ width: 80 }}></input>
+               </div>
+               <button className="button" type="submit">Cadastrar</button>
 
            </form>
        </div>
