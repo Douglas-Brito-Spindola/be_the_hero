42a7e16e5d620727e5ecd49a46fc92492942e574
Douglas Spíndola <douglas.spindola15@gmail.com>
2020-05-25 22:09:24 -0300

    { Melhoria } Adiconado para pegar o nome da entidade e criado estrutura para fazer comunicação com os itens criados para sere listados

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import {FiPower} from 'react-icons/fi';
 import {FiTrash2} from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css'
 import LogoImg from '../../assets/logo.svg';
 
 export default function Profile() {
+    const [incidents, setIncidents] = useState([]);
+
+
+    const ongId =  localStorage.getItem('ongId');
+    const ongName =  localStorage.getItem('ongName');
+    useEffect(() => {
+        api.get('profile', {
+            headers:{
+                Authorization: ongId
+            }
+        }).then(response =>{
+            setIncidents(response.data)
+        })
+
+    }, [ongId]);
+
     return(
         <div className="profile-container">
             <header>
                 <img src={LogoImg} alt="Be The Hero" />
-                <span> Bem vinda, APAD</span>
+                <span> Bem vinda, {ongName}</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
                 <button type="button">
