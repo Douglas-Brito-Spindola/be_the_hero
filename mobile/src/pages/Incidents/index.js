47902271311b458d40193df202b69d8584fb9d4a
Douglas Spíndola <douglas.spindola15@gmail.com>
2020-05-28 01:27:33 -0300

    { Criação } Criado index incidents

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, Text } from 'react-native';
+
+import logoImg from '../../assets/logo.png';
+
+import Styles from './styles';
+import styles from './styles';
 
 export default function Incidents() {
     return (
-        <View />
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={logoImg} />
+                <Text style={styles.headerText}>
+                    Total de 0 casos.
+                </Text>
+            </View>
+        </View>
     );
 }
+
